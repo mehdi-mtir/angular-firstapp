5443dfc938fac104e4d0d3bcb996b0cd766f9366
mehdi-mtir <mehdi.mtir@gmail.com>
2025-10-04 11:57:25 +0100

    Correction todo app with one component

--- src/app/todo/todo.ts
+++ src/app/todo/todo.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo.html',
   styleUrl: './todo.css'
 })
@@ -11,5 +12,50 @@ export class Todo {
     {id : 1, task : "Présenter les composants Angular", state:"done"},
     {id : 2, task : "Présenter les mécanisme de binding", state:"pending"},
     {id : 3, task : "Introduction aux directives", state:"pending"}
-  ]
+  ];
+  action = "";
+  taskToEdit = {
+    id : 0,
+    task : "",
+    state : ""
+  }
+
+  setAction(newAction : string){
+    this.action = newAction;
+  }
+
+  addTask(newTAsk : string){
+    const task = {
+      id : this.todos[this.todos.length - 1].id + 1,
+      task : newTAsk,
+      state : "pending"
+    }
+    this.todos = [...this.todos, task];
+    this.setAction("");
+  }
+
+  showEditForm(task : {id : number, task : string, state : string}){
+    console.log(task);
+    this.taskToEdit = {...task};
+    this.setAction("edit");
+  }
+
+  changeState(){
+    this.taskToEdit.state = (this.taskToEdit.state == 'done')?'pending':'done';
+  }
+
+  editTask(){
+    this.todos = this.todos.map(
+      todo => (todo.id == this.taskToEdit.id)?this.taskToEdit:todo
+    );
+    this.setAction("");
+  }
+
+  deleteTask(id :number){
+    if(confirm("Êtes-vous sûre de vouloir supprimer la tâche?"))
+      this.todos = this.todos.filter(
+        todo => todo.id != id
+      );
+  }
+
 }
